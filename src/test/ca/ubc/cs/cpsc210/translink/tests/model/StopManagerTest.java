0e94e7b45a676f8002c3c278b1541f899b91e9c3
sfyyuson <shaunyuson@gmail.com>
2017-11-08 16:16:52 -0800

    Fully tested StopManager

--- src/test/ca/ubc/cs/cpsc210/translink/tests/model/StopManagerTest.java
+++ src/test/ca/ubc/cs/cpsc210/translink/tests/model/StopManagerTest.java
@@ -2,11 +2,13 @@ package ca.ubc.cs.cpsc210.translink.tests.model;
 
 import ca.ubc.cs.cpsc210.translink.model.Stop;
 import ca.ubc.cs.cpsc210.translink.model.StopManager;
+import ca.ubc.cs.cpsc210.translink.model.exception.StopException;
 import ca.ubc.cs.cpsc210.translink.util.LatLon;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 
 /**
@@ -24,15 +26,21 @@ public class StopManagerTest {
         Stop s9999 = new Stop(9999, "My house", new LatLon(-49.2, 123.2));
         Stop r = StopManager.getInstance().getStopWithNumber(9999);
         assertEquals(s9999, r);
+        assertEquals(null, StopManager.getInstance().getSelected());
     }
 
     @Test
-    public void testDist() {
+    public void testDistAndGet() {
         Stop s1 = new Stop(1, "My house", new LatLon(0, 0));
         Stop s2 = new Stop(2, "My house", new LatLon(1000, 200));
         Stop s3 = new Stop(3, "My house", new LatLon(-50, -100));
+
         Stop r1 = StopManager.getInstance().getStopWithNumber(1, "My house", new LatLon(0, 0));
         assertEquals(s1, r1);
+        Stop r4 = StopManager.getInstance().getStopWithNumber(1);
+        assertEquals(s1, r4);
+        Stop r5 = StopManager.getInstance().getStopWithNumber(1, "My house", new LatLon(0, 0));
+        assertEquals(s1, r5);
 
         Stop r2 = StopManager.getInstance().getStopWithNumber(2, "My house", new LatLon(1000, 200));
         assertEquals(s2, r2);
@@ -44,5 +52,31 @@ public class StopManagerTest {
         assertEquals(s1, StopManager.getInstance().findNearestTo(new LatLon(0, 1)));
         assertEquals(s3, StopManager.getInstance().findNearestTo(new LatLon(-50, -50)));
         assertEquals(s2, StopManager.getInstance().findNearestTo(new LatLon(1000, 1)));
+    }
+
+    @Test
+    public void testSelect() {
+        Stop s1 = new Stop(1, "My house", new LatLon(0, 0));
+        Stop s2 = new Stop(2, "My house", new LatLon(1000, 200));
+        try {
+            StopManager.getInstance().setSelected(s1);
+            fail("Not expected");
+        } catch (StopException e) {
+            // expected
+        }
+        StopManager.getInstance().getStopWithNumber(2, "My house", new LatLon(1000, 200));
+        try {
+            StopManager.getInstance().setSelected(s2);
+        } catch (StopException e) {
+            fail("Not expected");
+        }
+        assertEquals(s2, StopManager.getInstance().getSelected());
+        StopManager.getInstance().clearSelectedStop();
+        assertEquals(null, StopManager.getInstance().getSelected());
+        for (Stop s : StopManager.getInstance()) {
+            if (s == s2) {
+                fail ("Not expected");
+            }
+        }
     }
 }
