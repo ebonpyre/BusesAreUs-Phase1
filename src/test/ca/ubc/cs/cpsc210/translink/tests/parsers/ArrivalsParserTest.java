bb8538e22ae7a9b40402e7aad0f144435539efad
sfyyuson <shaunyuson@gmail.com>
2017-11-08 18:23:04 -0800

    Exception handling done on all tests

--- src/test/ca/ubc/cs/cpsc210/translink/tests/parsers/ArrivalsParserTest.java
+++ src/test/ca/ubc/cs/cpsc210/translink/tests/parsers/ArrivalsParserTest.java
@@ -13,14 +13,21 @@ import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 
 /**
  * Test the ArrivalsParser
  */
 public class ArrivalsParserTest {
+
     @Test
-    public void testArrivalsParserNormal() throws JSONException, ArrivalsDataMissingException {
+    public void testConstructor() {
+        ArrivalsParser a = new ArrivalsParser();  // for the coverage XD
+    }
+
+    @Test
+    public void testArrivalsParserNormal() {
         Stop s = StopManager.getInstance().getStopWithNumber(51479);
         s.clearArrivals();
         String data = "";
@@ -30,12 +37,124 @@ public class ArrivalsParserTest {
             e.printStackTrace();
             throw new RuntimeException("Can't read the arrivals data");
         }
-        ArrivalsParser.parseArrivals(s, data);
+        try {
+            ArrivalsParser.parseArrivals(s, data);
+        } catch (JSONException e) {
+            fail("Not expected");
+        } catch (ArrivalsDataMissingException e) {
+            fail("Not expected");
+        }
         int count = 0;
         for (Arrival a : s) {
             assertTrue(a.getTimeToStopInMins() <= 120);
             count++;
         }
         assertEquals(6, count);
+    }
+
+    @Test
+    public void testArrivalsParserNotJSON() {
+        Stop s = StopManager.getInstance().getStopWithNumber(51479);
+        s.clearArrivals();
+        String data = "";
+        try {
+            data = new FileDataProvider("arrivalsnotjson.json").dataSourceToString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Can't read the arrivals data");
+        }
+        try {
+            ArrivalsParser.parseArrivals(s, data);
+            fail("Not expected");
+        } catch (JSONException e) {
+            // expected
+        } catch (ArrivalsDataMissingException e) {
+            fail("Not expected");
+        }
+        int count = 0;
+        for (Arrival a : s) {
+            assertTrue(a.getTimeToStopInMins() <= 120);
+            count++;
+        }
+        assertEquals(0, count);
+    }
+
+    @Test
+    public void testArrivalsParserMissingData() {
+        Stop s = StopManager.getInstance().getStopWithNumber(51479);
+        s.clearArrivals();
+        String data = "";
+        try {
+            data = new FileDataProvider("arrivalsmissingdata.json").dataSourceToString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Can't read the arrivals data");
+        }
+        try {
+            ArrivalsParser.parseArrivals(s, data);
+        } catch (JSONException e) {
+            fail("Not expected");
+        } catch (ArrivalsDataMissingException e) {
+            fail("Not expected");
+        }
+        int count = 0;
+        for (Arrival a : s) {
+            assertTrue(a.getTimeToStopInMins() <= 120);
+            count++;
+        }
+        assertEquals(4, count);
+    }
+
+    @Test
+    public void testArrivalsParserMissingSchedules() {
+        Stop s = StopManager.getInstance().getStopWithNumber(51479);
+        s.clearArrivals();
+        String data = "";
+        try {
+            data = new FileDataProvider("arrivalsmissingschedules.json").dataSourceToString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Can't read the arrivals data");
+        }
+        try {
+            ArrivalsParser.parseArrivals(s, data);
+        } catch (JSONException e) {
+            fail("Not expected");
+        } catch (ArrivalsDataMissingException e) {
+            fail("Not expected");
+        }
+        int count = 0;
+        for (Arrival a : s) {
+            assertTrue(a.getTimeToStopInMins() <= 120);
+            count++;
+        }
+        assertEquals(5, count);
+    }
+
+    @Test
+    public void testArrivalsParserArrivalException() {
+        Stop s = StopManager.getInstance().getStopWithNumber(51479);
+        s.clearArrivals();
+        String data = "";
+        try {
+            data = new FileDataProvider("arrivalsnodata.json").dataSourceToString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Can't read the arrivals data");
+        }
+        try {
+            ArrivalsParser.parseArrivals(s, data);
+            fail("Not expected");
+        } catch (JSONException e) {
+            fail("Not expected");
+        } catch (ArrivalsDataMissingException e) {
+            // expected
+        }
+        int count = 0;
+        for (Arrival a : s) {
+            assertTrue(a.getTimeToStopInMins() <= 120);
+            count++;
+        }
+        assertEquals(0, count);
     }
 }
